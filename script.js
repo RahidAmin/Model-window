12dd0bdc4920d73fb7b84c9bf6f2853ee9ee9ef6
RahidAmin <rahidamin19@gmail.com>
2024-01-15 21:40:40 +0600

    model window

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnCloseModel=document.querySelector('.close-modal');
+const btnShowModel=document.querySelectorAll('.show-modal');
+
+const openModal=function()
+{
+ console.log("button clicked");
+ modal.classList.remove('hidden');
+ overlay.classList.remove('hidden');     //display:block means visibl
+
+}
+
+for(let i=0;i<btnShowModel.length;i++)
+{
+  btnShowModel[i].addEventListener('click',openModal);
+   
+}
+const closeModal=function()
+  {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+  btnCloseModel.addEventListener('click',closeModal);
+  overlay.addEventListener('click',closeModal);
+
+
+  document.addEventListener('keydown',function(e)
+  {
+    if(e.key==='Escape' && !modal.classList.contains('hidden'))
+    {
+     
+        closeModal();
+
+    }
+  })
+ 
